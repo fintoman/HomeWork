19556c5a79609902a7510b65927ded7b3defcbdf
Anton Kovalchuk <fintoman18@gmail.com>
2023-11-07 02:19:31 +0200

    finsh 21 модуль

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// 1
+// let name = "Іван";
+// let city = "Kyiv";
+// city = name;
+// console.log(city); //Іван
+
+// 2
+// let name = "Olga";
+// console.log(`привіт ${1}`); // привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`); // привіт Olga
+
+//*************3************** *
+
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+// console.log(typeof Number(a));
+// console.log(typeof Number.parseInt(b));
+// console.log(typeof Number.parseFloat(c));
+
+//**************4 */
+
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+////************5 */
+// console.log(Math.max(20, 10, 50, 40)); //50
+
+///*********6 */
+// console.log(Math.random() * (4 - 2) + 2); //3.9111606310738076
+
+//********7 */
+//const message = "Welcome to Bahamas!";
+//console.log(message.length); //19
+
+//******8 */
+//const message = "Welcome to Bahamas!";
+//console.log(message.toUpperCase());
+
+//******9 */
+// let user = {};
+// user.name = "Anton";
+// user.age = "31";
+// user.city = "Kyiv";
+// // console.log(user);
+// delete user.city;
+// user["like flowers"] = true;
+// // console.log(user);
+
+// //***10**
+// // За допомогою циклу “for…in” вивести в консоль ключі і значення об*єкта
+
+// for (key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+// }
