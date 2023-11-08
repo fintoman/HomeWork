0fea57c4f041e4ef19a7965598d12f507824fba8
Anton Kovalchuk <fintoman18@gmail.com>
2023-11-08 15:39:17 +0200

    2.3

---
+++ js/scriptLesson2.js
@@ -0,0 +1,39 @@
+//**********1 */
+// Якщо змінна більше нуля - виведіть true, менше - false
+// let x = 1;
+// let x = 0;
+// let x = -3;
+// if (x > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//******2 */
+//Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',"qwerty", true
+
+//let y = "test";
+//let y = "qwerty";
+// let y = "true";
+// if (y === "test") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13
+
+// let z = 1;
+//let z = 10;
+//let z = 13;
+
+// if (z > 10) {
+//   console.log(z - 5);
+// } else if (z < 10)
+// {
+//   console.log(z + 5);
+// }
+
