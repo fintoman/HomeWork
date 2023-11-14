6c8c15b6ba409d5b55dc375b5c84b18cb51d0f59
Anton Kovalchuk <fintoman18@gmail.com>
2023-11-14 16:49:08 +0200

    3.2

--- js/scriptLesson3.js
+++ js/scriptLesson3.js
@@ -1,8 +1,45 @@
-const fruts = [
-  { id: 0, name: "Apple" },
-  { id: 1, name: "Tomat" },
-  { id: 2, name: "Cherry" },
-  { id: 3, name: "Orange" },
-];
+// ********* 1 *****
+// const fruts = [
+//   { id: 0, name: "Apple" },
+//   { id: 1, name: "Tomat" },
+//   { id: 2, name: "Cherry" },
+//   { id: 3, name: "Orange" },
+// ];
 
-console.log(fruts);
+// let frutsNew = fruts.map((fruts) => fruts.name);
+
+// console.log(frutsNew);
+
+// ********* 2 *****
+// for (let i = 1; i < 11; i++) {
+//   if (i % 2 === 1) continue;
+//   console.log(i);
+// }
+
+// ********* 3 *****
+//Замініть цикл "for" на "while"
+// for (let i = 0; i < 5; i++) {
+// console.lpg( `цифра ${i}!` );
+// }
+
+// let i = 0;
+// while (i < 5) {
+//   i++;
+//   console.log(`цифра ${i}!`);
+// }
+
+// ********* 4 *****
+
+//Напишіть цикл, який пропонує prompt ввести число більше за 100.
+//Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
+//Цикл повинен запитувати число доти, доки відвідувач не введе число,
+// більше за 100, або не скасує ввід/введе порожній рядок.
+
+let massa = 0;
+
+while (value <= 100);
+{
+  let value = +prompt("Введіть число більше 100");
+
+  console.log("Ви ввели число більше за 100", value);
+}
