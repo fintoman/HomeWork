4f39a0444a0a3ac0246c4faf6066f39f74213c84
Anton <fintoman18@gmail.com>
2023-11-19 21:25:17 +0200

    5.1

---
+++ js/scriptLesson5.js
@@ -0,0 +1,51 @@
+// --1--
+// 1) написати об*єкт студента який буде виводити ім*я, спеціальнісь, середній
+// бал і кількість пропущених занять
+// 2) написати метод який буде виводити цю інформацію
+// 3) написати три варіанти студентів
+// 4) прикріпити знначення за допомогою call apply bind
+
+// const student = {
+//   name: "Anton",
+//   spec: "Engineer",
+//   bal: "99",
+//   epsent: "0",
+//   showinfo: function () {
+//     console.log(
+//       "name " +
+//         this.name +
+//         " spec " +
+//         this.spec +
+//         " bal " +
+//         this.bal +
+//         " epsent " +
+//         this.epsent
+//     );
+//   },
+// };
+
+// studentTwo = {
+//   name: "Anna",
+//   spec: "Dev",
+//   bal: "88",
+//   epsent: "5",
+// };
+
+// studentFree = {
+//   name: "Vova",
+//   spec: "Reserch",
+//   bal: "90",
+//   epsent: "4",
+// };
+
+// studentFor = {
+//   name: "Lesya",
+//   spec: "HR",
+//   bal: "93",
+//   epsent: "4",
+// };
+
+// student.showinfo();
+// student.showinfo.bind(studentTwo)();
+// student.showinfo.call(studentFree);
+// student.showinfo.apply(studentFor);
