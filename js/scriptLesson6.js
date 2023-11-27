83e5fd35774bdb43664b836931d293b6191411dd
Anton Kovalchuk <fintoman18@gmail.com>
2023-11-27 23:50:01 +0200

    1 finish

--- js/scriptLesson6.js
+++ js/scriptLesson6.js
@@ -4,9 +4,33 @@
 // створити три різних юзери
 // вивести данні
 
-class Abonent {
-  constructor(props) {
-    this.firstName = "";
-    this.number = "";
-  }
-}
+// class Abonent {
+//   constructor({ name }) {
+//     this.firstName = "";
+//     this.number = "";
+//     this.name = name;
+//   }
+//   set name(newName) {
+//     const nameRow = newName.split(" ");
+//     this.firstName = nameRow[0];
+//     this.number = nameRow[1];
+//   }
+//   get name() {
+//     return `firstName: ${this.firstName} number: ${this.number}`;
+//   }
+// }
+
+// const Alex = new Abonent({
+//   name: "Alex 099-982-09-09",
+// });
+
+// const Olena = new Abonent({
+//   name: "Olena 099-982-09-08",
+// });
+// const Bogdan = new Abonent({
+//   name: "Bogdan 099-982-09-07",
+// });
+
+// console.log(Alex.name);
+// console.log(Olena.name);
+// console.log(Bogdan.name);
