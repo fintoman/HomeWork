147503b25b8b10c9299cd6a6af84eb6fc7919f8d
Anton Kovalchuk <fintoman18@gmail.com>
2023-12-01 02:47:32 +0200

    7.3

--- js/scriptLesson7.js
+++ js/scriptLesson7.js
@@ -6,25 +6,25 @@
 // sum() повертає суму цих властивостей.
 // mul() повертає результат множення даних властивостей.
 
-function Calculator() {
-  this.one = 0;
-  this.two = 0;
+// function Calculator() {
+//   this.one = 0;
+//   this.two = 0;
 
-  this.read = function () {
-    this.one = +prompt("Введіть перше значення", 0);
-    this.two = +prompt("Введіть друге значення", 0);
-  };
+//   this.read = function () {
+//     this.one = +prompt("Введіть перше значення", 0);
+//     this.two = +prompt("Введіть друге значення", 0);
+//   };
 
-  this.sum = function () {
-    return this.one + this.two;
-  };
-  this.mul = function () {
-    return this.one * this.two;
-  };
-}
+//   this.sum = function () {
+//     return this.one + this.two;
+//   };
+//   this.mul = function () {
+//     return this.one * this.two;
+//   };
+// }
 
-let calculator = new Calculator();
-calculator.read();
+// let calculator = new Calculator();
+// calculator.read();
 
-console.log("Сума = " + calculator.sum());
-console.log("Добуток = " + calculator.mul());
+// console.log("Сума = " + calculator.sum());
+// console.log("Добуток = " + calculator.mul());
