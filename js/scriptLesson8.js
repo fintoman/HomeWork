62c76874975218c82db6fac2033574fbfa4db3bc
Anton Kovalchuk <fintoman18@gmail.com>
2023-12-21 02:08:52 +0200

    9.0

--- js/scriptLesson8.js
+++ js/scriptLesson8.js
@@ -8,5 +8,5 @@
 // </body>
 // </html>
 
-let element = document.querySelector("[data-widget-name]");
-alert(element.getAttribute("data-widget-name"));
+// let element = document.querySelector("[data-widget-name]");
+// alert(element.getAttribute("data-widget-name"));
