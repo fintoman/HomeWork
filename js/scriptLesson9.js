289d535d57e7df8ce75a281d731f3e6c7e61c106
Anton Kovalchuk <fintoman18@gmail.com>
2023-12-22 04:17:53 +0200

    9.2

--- js/scriptLesson9.js
+++ js/scriptLesson9.js
@@ -2,7 +2,30 @@
 // Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент
 // <div id="text"> зникав.
 
+// const btn = document.querySelector("#btn1");
+// const textElement = document.querySelector("#text");
+// btn.addEventListener("click", () => {
+//   textElement.style.visibility = "hidden";
+// });
+
+//Другий вариант
+// btn.addEventListener("click", () => {
+//   textElement.style.display = "none";
+// });
+
 // -----2----
 // Напишіть такий код, щоб після натискання на кнопку, вона зникала.
+
+// const btn = document.querySelector("#btn1");
+// btn.addEventListener("click", () => {
+//   btn.style.visibility = "hidden";
+// });
+
+// Другий вариант
+// btn.addEventListener("click", () => {
+//   btn.style.display = "none";
+// });
+
 // -----3----
+
 // Створіть дерево, яке показує/приховує дочірні вузли при кліці
